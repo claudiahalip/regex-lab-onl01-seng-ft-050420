a5cc69820f4aa88dd73d866b11a5ae7af07cd278
claudia v <claudia4ro@yahoo.com>
2020-05-17 05:40:58 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -27,7 +27,7 @@ end
 
 
 def valid_phone_number?(phone)
-  phone.match( /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/) true : false
+  phone.match( /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/)?  true : false
    
 
 end
