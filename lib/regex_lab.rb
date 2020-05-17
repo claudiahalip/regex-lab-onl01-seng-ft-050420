15faf3a817e844d8f1a8d55514fa1241f1176337
claudia v <claudia4ro@yahoo.com>
2020-05-17 05:40:37 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -27,8 +27,7 @@ end
 
 
 def valid_phone_number?(phone)
-  if phone.match( /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/)
-    true
-  end
+  phone.match( /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/) true : false
+   
 
 end
