a26b1ec0d8c8299c888149aa7e19dc8f71608080
claudia v <claudia4ro@yahoo.com>
2020-05-17 05:44:33 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,30 @@
+require 'pry'
 def starts_with_a_vowel?(word)
-
+  word.start_with?(/^[aeiouAEIOU]/)
 end
+
+
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un[a-z]*?ing?/)
+  
 end
+
 
 def words_five_letters_long(text)
-
+  m_3 = text.scan(/\b\w{5}\b/)
+ 
+ 
 end
+
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z].*[a-zA-Z][.?!]$/)? true:false
 end
 
+
 def valid_phone_number?(phone)
+  phone.match( /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/)? true : false
+   
 
 end
